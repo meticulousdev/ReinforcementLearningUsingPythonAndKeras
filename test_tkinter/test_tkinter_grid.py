244d27dd705c66403fb11396f0f1924fe0ec1baa
meticulousdev <meticulousdev@gmail.com>
2024-01-22 01:01:30 +0900

    test grid

--- test_tkinter/test_tkinter_grid.py
+++ test_tkinter/test_tkinter_grid.py
@@ -1,49 +1,26 @@
 import tkinter as tk
 from tkinter import Button
 
-HEIGHT = 400
-WIDTH = 640
-
-
-def func_counter() -> None:
-    func_counter.cnt += 1
-    print(f"cnt: {func_counter.cnt}")
+UNIT = 100
+HEIGHT = 5
+WIDTH = 5
 
 
 if __name__ == "__main__":
     window = tk.Tk()
-    window.geometry(f"{WIDTH}x{HEIGHT}")
-    window.title("Test Mainloop")
+    window.geometry(f"{WIDTH * UNIT}x{HEIGHT * UNIT}")
+    window.title("Test Grid")
 
     canvas = tk.Canvas(window, bg='white',
-                       height=HEIGHT,
-                       width=WIDTH)
-
-    # (0, 0)
-    label = tk.Label(window, text="(0, 0)", width=50, height=5, fg="black", bg="white")
-    canvas.create_window(0 + 50, 0 + 50, window=label)
-
-    # (0, HEIGHT)    
-    label = tk.Label(window, text="(0, HEIGHT)", width=50, height=5, fg="black", bg="white")
-    canvas.create_window(0 + 50, HEIGHT - 50, window=label)
+                       height=HEIGHT * UNIT,
+                       width=WIDTH * UNIT)
     
-    # (WIDTH, 0)
-    label = tk.Label(window, text="(WIDTH, 0)", width=50, height=5, fg="black", bg="white")
-    canvas.create_window(WIDTH - 50, 0 + 50, window=label)
-    
-    # (WIDTH, HEIGHT)
-    label = tk.Label(window, text="(WIDTH, HEIGHT)", width=50, height=5, fg="black", bg="white")
-    canvas.create_window(WIDTH - 50, HEIGHT - 50, window=label)
-    
-    label = tk.Label(window, text="Click Button", width=10, height=5, fg="black", bg="white")
-    canvas.create_window(WIDTH - 200, HEIGHT - 300, window=label)
-
-    canvas.create_line(WIDTH - 200, HEIGHT - 200, WIDTH - 200, HEIGHT - 300)
-
-    func_counter.cnt: int = 0
-    test_button = Button(window, text="Button", command=func_counter)
-    test_button.configure(width=10, activebackground="#33B5E5")
-    canvas.create_window(WIDTH - 200, HEIGHT - 150, window=test_button)
+    for col in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
+        x0, y0, x1, y1 = col, 0, col, HEIGHT * UNIT
+        canvas.create_line(x0, y0, x1, y1, fill='black')
+    for row in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
+        x0, y0, x1, y1 = 0, row, HEIGHT * UNIT, row
+        canvas.create_line(x0, y0, x1, y1, fill='black')
 
     canvas.pack()
 

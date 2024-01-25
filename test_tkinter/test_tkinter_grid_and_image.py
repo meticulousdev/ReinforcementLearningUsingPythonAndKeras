c12f0b7bf048dd77d55d4dcda4232bbb9729bbf6
meticulousdev <meticulousdev@gmail.com>
2024-01-25 23:59:29 +0900

    rectangle

--- test_tkinter/test_tkinter_grid_and_image.py
+++ test_tkinter/test_tkinter_grid_and_image.py
@@ -11,41 +11,35 @@ WIDTH = 5
 PhotoImage = ImageTk.PhotoImage
 
 
-class GraphicDisplay(tk.Tk):
-    def __init__(self):
-        super(GraphicDisplay, self).__init__()
-        (self.up, self.down, self.left, self.right), self.shapes = self.load_images()
-
-    def load_images(self):
-        file_path: str = "./chap01/img/"
-        up = PhotoImage(Image.open(file_path + "up.png").resize((13, 13)))
-        right = PhotoImage(Image.open(file_path + "right.png").resize((13, 13)))
-        left = PhotoImage(Image.open(file_path + "left.png").resize((13, 13)))
-        down = PhotoImage(Image.open(file_path + "down.png").resize((13, 13)))
-        rectangle = PhotoImage(Image.open(file_path + "rectangle.png").resize((65, 65)))
-        triangle = PhotoImage(Image.open(file_path + "triangle.png").resize((65, 65)))
-        circle = PhotoImage(Image.open(file_path + "circle.png").resize((65, 65)))
-        return (up, down, left, right), (rectangle, triangle, circle)
-
-
 if __name__ == "__main__":
-    # window = tk.Tk()
-    # file_path: str = "./chap01/img/"
-    # up = PhotoImage(Image.open(file_path + "up.png").resize((13, 13)))
-
-    gd = GraphicDisplay()
-    print(gd.up)
-    # print(type(gd.up))
-
     window = tk.Tk()
     window.geometry(f"{WIDTH * UNIT}x{HEIGHT * UNIT}")
-    window.title("Test Grid")
+    window.title("Test Grid and Imgae")
+
+    file_path: str = "./chap01/img/"
+    up = PhotoImage(Image.open(file_path + "up.png").resize((13, 13)))
+    right = PhotoImage(Image.open(file_path + "right.png").resize((13, 13)))
+    left = PhotoImage(Image.open(file_path + "left.png").resize((13, 13)))
+    down = PhotoImage(Image.open(file_path + "down.png").resize((13, 13)))
+    rectangle = PhotoImage(Image.open(file_path + "rectangle.png").resize((65, 65)))
+    triangle = PhotoImage(Image.open(file_path + "triangle.png").resize((65, 65)))
+    circle = PhotoImage(Image.open(file_path + "circle.png").resize((65, 65)))
 
     canvas = tk.Canvas(window, bg='white',
                        height=HEIGHT * UNIT,
                        width=WIDTH * UNIT)
+    
+    # Grid
+    for col in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
+        x0, y0, x1, y1 = col, 0, col, HEIGHT * UNIT
+        canvas.create_line(x0, y0, x1, y1, fill='black')
+    for row in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
+        x0, y0, x1, y1 = 0, row, HEIGHT * UNIT, row
+        canvas.create_line(x0, y0, x1, y1, fill='black')
 
-    canvas.create_image(50, 50, image=gd.shapes[0])
+    canvas.create_image(50, 150, image=rectangle)
+    canvas.create_image(150, 150, image=rectangle)
+    canvas.create_image(250, 150, image=rectangle)
     
     canvas.pack()
 
